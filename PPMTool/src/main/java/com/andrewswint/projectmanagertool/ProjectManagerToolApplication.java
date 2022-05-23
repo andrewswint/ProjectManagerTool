bf7c43fe081de4a9733c34ea531719194ededb9d
andrewswint <andrewswint1976@gmail.com>
2022-05-23 16:38:07 -0700

    initial commit for new project manager tool

---
+++ PPMTool/src/main/java/com/andrewswint/projectmanagertool/ProjectManagerToolApplication.java
@@ -0,0 +1,13 @@
+package com.andrewswint.projectmanagertool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagerToolApplication.class, args);
+    }
+
+}
---
+++ PPMTool/src/test/java/com/andrewswint/projectmanagertool/ProjectManagerToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrewswint.projectmanagertool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
